06337136a29d1f9172363578bf5e3280677fb36d
Dominic Tillison <22tilldj@gmail.com>
2025-06-18 12:04:58 -0400

    added my post nav bar and css

--- src/components/MyPost/mypost.jsx
+++ src/components/MyPost/mypost.jsx
@@ -1,9 +1,60 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { getMyPosts } from "../../services/postService.js";
+import { Link } from "react-router-dom";
+import "./myposts.css";
 
-const myPost = () => {
-  const [post, setPost] = useState({})
+export const MyPost = ({ token }) => {
+  const [post, setPost] = useState([]);
+  const [orderedMyPosts, setOrderedMyPosts] = useState([]);
+
+  const today = new Date();
 
   useEffect(() => {
-    
-  })
-}
+    getMyPosts(token).then(setPost);
+  }, [token]);
+
+  useEffect(() => {
+    const orderMyPosts = post?.toSorted(
+      (a, b) => new Date(a.publication_date) - new Date(b.publication_date)
+    );
+    setOrderedMyPosts(orderMyPosts);
+  }, [post]);
+
+  return (
+    <>
+      <div>
+        <article id="all-posts-container">
+          <div id="posts-table">
+            <div className="row">
+              <div className="column">Title</div>
+              <div className="column">Author</div>
+              <div className="column">Date</div>
+              <div className="column">Category</div>
+              <div className="column">Tags</div>
+            </div>
+            {orderedMyPosts?.map((post) => {
+              if (
+                post.approved === 1 &&
+                new Date(post.publication_date) < today
+              ) {
+                return (
+                  <div className="row" key={post.id}>
+                    <div className="column">
+                      <Link target="_blank" to={`./posts/${post.id}`}>
+                        {post.title}
+                      </Link>
+                    </div>
+                    <div className="column">{post.author}</div>
+                    <div className="column">{post.publication_date}</div>
+                    <div className="column">{post.category}</div>
+                    <div className="column">Tags</div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </article>
+      </div>
+    </>
+  );
+};
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,5 +1,9 @@
 export const getAllPosts = () => {
       return fetch("http://localhost:8088/posts").then((res) =>
-    res.json()
+   res.json()
   );
+}
+
+export const getMyPosts = (token) => {
+  return fetch (`http://localhost:8088/myposts/${token}`).then((res) => res.json())
 }
