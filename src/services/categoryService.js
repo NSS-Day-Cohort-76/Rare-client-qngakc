b1ecb14d64640bfe34d22501233c5729cc9b42c2
Luke Davids <lukedavids@gmail.com>
2025-06-19 15:29:53 -0500

    All categories + Delete category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,15 +1,22 @@
-export const createCategory= (category) => {
-    return fetch(`http://localhost:8088/categories`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(category)
-    }).then(res => res.json())
-}
-
+export const createCategory = (category) => {
+  return fetch(`http://localhost:8088/categories`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(category),
+  }).then((res) => res.json());
+};
 
 export const getAllCategories = () => {
-    return fetch(`http://localhost:8088/categories`).then((res) => res.json())
+  return fetch(`http://localhost:8088/categories`).then((res) => res.json());
+};
 
-}
+export const deleteCategory = (pk) => {
+  return fetch(`http://localhost:8088/categories/${pk}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
