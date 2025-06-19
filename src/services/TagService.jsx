58bf133765f137a4213096e592772af3d1698f44
Brit Beaty <britneybeaty@gmail.com>
2025-06-19 11:28:03 -0500

    added DeleteTag functionality to AllTags

--- src/services/TagService.jsx
+++ src/services/TagService.jsx
@@ -13,3 +13,9 @@ export const getAllTags = () => {
     return fetch(`http://localhost:8088/tags`).then((res) => res.json())
 
 }
+
+export const deleteTag = (tagId) => {
+    return fetch(`http://localhost:8088/tags/${tagId}`, {
+        method: "DELETE",
+    })
+}
