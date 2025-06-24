b52a1d1f6ebf59d4752cc688ceb168d431cf0b51
Brit Beaty <britneybeaty@gmail.com>
2025-06-24 10:40:23 -0500

    starting editCategory

--- src/components/categories/AllCategories.jsx
+++ src/components/categories/AllCategories.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import {
   deleteCategory,
   getAllCategories,
+  updateCategory,
 } from "../../services/categoryService";
 import { CreateNewCategory } from "./CreateCategory";
 import "./Categories.css";
 
 export const AllCategories = () => {
   const [allCategories, setAllCategories] = useState([]);
+  const [editingCategory, setEditingCategory] = useState(null)
+  const [editLabel, setEditLabel] = useState("")
 
   useEffect(() => {
     getAllCategories().then(setAllCategories);
@@ -20,9 +23,47 @@ export const AllCategories = () => {
       });
     }
   };
+
+  const handleEditClick = (category) => {
+    setEditingCategory(category)
+    setEditLabel(category.label)
+  }
+
+
+  const handleCancelEdit = () => {
+    setEditingCategory(null)
+    setEditLabel("")
+    refreshCategories()
+  }
+
+
+  const handleUpdateSubmit = (event) => {
+    event.preventDefault()
+
+    if (editLabel.trim() === "") return
+
+    updateCategory(editingCategory.id, { label: editLabel }).then(() => {
+
+      setAllCategories((prevCats) =>
+        prevCats.map((cat) =>
+          cat.id === editingCategory.id ? { ...cat, label: editLabel } : cat
+        )
+      )
+
+      setEditingCategory(null)
+      setEditLabel("")
+    }).catch((err) => {
+      alert("Failed to update tag: " + err.message)
+    })
+  }
+
+  const refreshCategories = () => getAllCategories().then(setAllCategories)
+
   return (
     <div className="tags-table">
+
       <div className="tags-row">
+
         <div className="tags-column">
           <h2 className="title is-2">Categories</h2>
           {allCategories.map((cat) => (
@@ -44,14 +85,54 @@ export const AllCategories = () => {
 
         <div className="tags-column">
           <div className="create-tag">
-            <CreateNewCategory
-              onCategoryCreated={(newCat) =>
-                setAllCategories((prev) => [...prev, newCat])
-              }
-            />
+            {!editingCategory && (
+              <CreateNewCategory
+                onCategoryCreated={(newCat) =>
+                  setAllCategories((prev) => [...prev, newCat])
+                }
+              />
+            )}
+
+            {editingCategory && (
+              <div className="create-category">
+                <h3 className="title is=4">Edit Tag</h3>
+
+                <form>
+                  <div className="field">
+                    <input className="input"
+                      type="text"
+                      value={editLabel}
+                      onChange={(event) => setEditLabel(event.target.value)}
+                      placeholder="Edit Category Label"
+                    />
+                  </div>
+
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <button className="button is-primary" type="button" onClick={handleUpdateSubmit}>
+                        Update Category
+                      </button>
+                    </div>
+                    <div className="control">
+                      <button
+                        className="button is-light"
+                        type="button"
+                        onClick={handleCancelEdit}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+
+                </form>
+              </div>
+              )
+            }
           </div>
         </div>
+      
       </div>
+
     </div>
   );
 };
